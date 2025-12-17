2faba4bc0991c88ebfb97446fcd999cf3248a108
Alfredo Inostroza <jadenguy@gmail.com>
2025-12-16 20:23:04 -0800

    progress

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -13,5 +13,53 @@ pub fn parse_id_range(s: &str) -> (i32, i32) {
         Some(s) => s.parse::<i32>().unwrap(),
         None => 0,
     };
+    if start_range > end_range {
+        return (0, 0);
+    }
     (start_range, end_range)
+}
+pub fn find_invalid_ids(range: (i32, i32)) -> Vec<i32> {
+    let lower_bound = range.0;
+    let upper_bound = range.1;
+    let mut lower_order_of_magnitude = lower_bound.ilog10();
+    let mut upper_order_of_magnitude = upper_bound.ilog10();
+    let mut results: Vec<i32> = vec![];
+    static BASE_10: i32 = 10;
+    if lower_order_of_magnitude % 2 == 0 {
+        lower_order_of_magnitude += 1
+    }
+    if upper_order_of_magnitude % 2 == 0 {
+        upper_order_of_magnitude -= 1
+    }
+    let low_pow = BASE_10.pow(lower_order_of_magnitude / 2 + 1);
+    let mut lowest_invalid_id_half = lower_bound / low_pow;
+    let upper_half_domain = BASE_10.pow(upper_order_of_magnitude / 2 + 1);
+    let mut highest_invalid_id_half = upper_bound / upper_half_domain;
+    let mut lowest_invalid_id = lowest_invalid_id_half * low_pow + lowest_invalid_id_half;
+    if lowest_invalid_id < lower_bound {
+        lowest_invalid_id_half += 1;
+    }
+    lowest_invalid_id = lowest_invalid_id_half * low_pow + lowest_invalid_id_half;
+
+    let mut highest_invalid_id =
+        highest_invalid_id_half * upper_half_domain + highest_invalid_id_half;
+    if highest_invalid_id > upper_bound {
+        highest_invalid_id_half -= 1;
+    }
+    highest_invalid_id = highest_invalid_id_half * upper_half_domain + highest_invalid_id_half;
+    println!("{} domain to {}", low_pow, upper_half_domain);
+    println!("{} value to {}", lowest_invalid_id, highest_invalid_id);
+    if lowest_invalid_id > highest_invalid_id {
+        println!("invalid");
+        return results;
+    }
+    let domains: u32 = (lower_bound..=upper_bound)
+        .filter(|b| b % 2 == 1)
+        .map(|b| b);
+    for domain in domains {
+        for value in (BASE_10.pow(domain))..(BASE_10.pow(domain + 1)) {
+            results.push(value);
+        }
+    }
+    return results;
 }
--- tests/day2.rs
+++ tests/day2.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use aoc2025::day2::{parse_id_range, parse_id_ranges};
+    use aoc2025::day2::{find_invalid_ids, parse_id_range, parse_id_ranges};
     #[test]
     fn parse_range_11_22() {
         // arrange
@@ -38,12 +38,48 @@ mod tests {
         )
     }
     #[test]
-    fn find_repeat_ranges_11_22() {
+    fn find_invalid_ids_in_range_10_10() {
         // arrange
-        let id_ranges = "11-22";
+        let range = (10, 10);
         // act
-        let parsed_range = parse_id_range(id_ranges);
+        let invalid_ids = find_invalid_ids(range);
         // assert
-        assert_eq!(parsed_range, (11, 22))
+        assert_eq!(invalid_ids, [])
+    }
+    #[test]
+    fn find_invalid_ids_in_range_111_111() {
+        // arrange
+        let range = (111, 111);
+        // act
+        let invalid_ids = find_invalid_ids(range);
+        // assert
+        assert_eq!(invalid_ids, [])
+    }
+    #[test]
+    fn find_invalid_ids_in_range_11_11() {
+        // arrange
+        let range = (11, 11);
+        // act
+        let invalid_ids = find_invalid_ids(range);
+        // assert
+        assert_eq!(invalid_ids, [11])
+    }
+    #[test]
+    fn find_invalid_ids_in_range_11_22() {
+        // arrange
+        let range = (11, 22);
+        // act
+        let invalid_ids = find_invalid_ids(range);
+        // assert
+        assert_eq!(invalid_ids, [11])
+    }
+    #[test]
+    fn find_invalid_ids_in_range_1_2() {
+        // arrange
+        let range = (1, 2);
+        // act
+        let invalid_ids = find_invalid_ids(range);
+        // assert
+        assert_eq!(invalid_ids, [])
     }
 }
