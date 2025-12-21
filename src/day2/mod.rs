f47ccf887f82ed6ca5881b9188ac7e824eba7ddc
Alfredo Inostroza <jadenguy@gmail.com>
2025-12-20 18:55:46 -0800

    day 2 part 1 done

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,72 +1,14 @@
-pub fn parse_id_ranges(id_ranges_string: &str) -> Vec<(u64, u64)> {
-    id_ranges_string.split(",").map(parse_id_range).collect()
-}
-pub fn parse_id_range(s: &str) -> (u64, u64) {
-    let x: Vec<String> = s.trim().split("-").map(|s| s.to_string()).collect();
-    let start_string = x.first();
-    let start_range: u64 = match start_string {
-        Some(s) => s.parse::<u64>().unwrap(),
-        None => 0,
-    };
-    let end_string = x.last();
-    let end_range: u64 = match end_string {
-        Some(s) => s.parse::<u64>().unwrap(),
-        None => 0,
-    };
-    if start_range > end_range {
-        return (0, 0);
-    }
-    (start_range, end_range)
-}
-pub fn find_invalid_ids(range: (u64, u64)) -> Vec<u64> {
-    let lower_bound = range.0;
-    let upper_bound = range.1;
-    let mut lower_order_of_magnitude = lower_bound.ilog10();
-    let mut upper_order_of_magnitude = upper_bound.ilog10();
-    let results: Vec<u64> = vec![];
-    static BASE_10: u64 = 10;
-    if lower_order_of_magnitude % 2 == 0 {
-        lower_order_of_magnitude += 1;
-    }
-    if upper_order_of_magnitude % 2 == 0 {}
-    upper_order_of_magnitude -= 1;
-    let low_pow = BASE_10.pow(lower_order_of_magnitude / 2 + 1);
-    let mut lowest_invalid_id_half = lower_bound / low_pow;
-    let upper_half_domain = BASE_10.pow(upper_order_of_magnitude / 2 + 1);
-    let mut highest_invalid_id_half = upper_bound / upper_half_domain;
-    let mut lowest_invalid_id = lowest_invalid_id_half * low_pow + lowest_invalid_id_half;
-    if lowest_invalid_id < lower_bound {
-        lowest_invalid_id_half += 1;
-    }
-    lowest_invalid_id = lowest_invalid_id_half * low_pow + lowest_invalid_id_half;
-
-    let mut highest_invalid_id =
-        highest_invalid_id_half * upper_half_domain + highest_invalid_id_half;
-    if highest_invalid_id > upper_bound {
-        highest_invalid_id_half -= 1;
-    }
-    highest_invalid_id = highest_invalid_id_half * upper_half_domain + highest_invalid_id_half;
-    print!("{} domain to {}", low_pow, upper_half_domain);
-    print!("{} value to {}", lowest_invalid_id, highest_invalid_id);
-    if lowest_invalid_id > highest_invalid_id {
-        print!("invalid");
-        return results;
-    }
-    // let domains: u32 = (lower_bound..=upper_bound)
-    //     .filter(|b| b % 2 == 1)
-    //     .map(|b| b as i32);
-    // for domain in domains {
-    //     for value in (BASE_10.pow(domain))..(BASE_10.pow(domain + 1)) {
-    //         results.push(value);
-    //     }
-    // }
-    return results;
-}
 pub fn find_invalid_ids_lexicographically(range: &str, verbose: bool) -> Vec<u64> {
     if verbose {
         print!("{} check; ", range);
     }
     let (first, last) = range.trim().split_once("-").unwrap();
+    if (first.len() == last.len()) && (first.len() % 2 == 1) {
+        if verbose {
+            println!("{} is in an odd range and cannot have results.", range)
+        }
+        return vec![];
+    }
     let start_dup_range = lexicographical_lower_bound(first, verbose);
     let end_dup_range = lexicographical_upper_bound(last, verbose);
     if end_dup_range < start_dup_range {
@@ -80,9 +22,18 @@ pub fn find_invalid_ids_lexicographically(range: &str, verbose: bool) -> Vec<u64
         .collect();
     if verbose && !result.is_empty() {
         print!("{} has invalid IDs ", range);
-        for v in &result {
-            print!(",{}", v)
-        }
+
+        print!(
+            "{}",
+            result
+                .iter()
+                .map(|y| y.to_string())
+                .collect::<Vec<String>>()
+                .join(", ")
+        );
+        // for v in &result {
+        //     print!(",{}", v)
+        // }
     }
     println!();
     result
@@ -120,7 +71,7 @@ fn lexicographical_lower_bound(num_str: &str, verbose: bool) -> u64 {
 fn lexicographical_upper_bound(num_str: &str, verbose: bool) -> u64 {
     let (high, low) = num_str.split_at(num_str.len() / 2);
     if high.len() < low.len() {
-        let result = ("9".repeat(low.len() / 2))
+        let result = ("9".repeat(low.len() - 1))
             .parse::<u64>()
             .unwrap_or_default();
         if verbose {
--- tests/day2.rs
+++ tests/day2.rs
@@ -11,6 +11,15 @@ mod tests {
         assert_eq!(invalid_ids, [1010])
     }
     #[test]
+    fn find_invalid_ids_lexicographically_111_222() {
+        // arrange
+        let id_range = "111-222";
+        // act
+        let invalid_ids = find_invalid_ids_lexicographically(id_range, true);
+        // assert
+        assert_eq!(invalid_ids, [])
+    }
+    #[test]
     fn find_invalid_ids_lexicographically_4487_9581() {
         // arrange
         let id_range = "4487-9581";
@@ -89,86 +98,4 @@ mod tests {
         // assert
         assert_eq!(invalid_ids, 1227775554)
     }
-
-    // #[test]
-    // fn parse_range_11_22() {
-    //     // arrange
-    //     let id_ranges = "11-22";
-    //     // act
-    //     let parsed_range = parse_id_range(id_ranges);
-    //     // assert
-    //     assert_eq!(parsed_range, (11, 22))
-    // }
-
-    // #[test]
-    // fn parse_() {
-    //     // arrange
-    //     let id_ranges = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
-    //     1698522-1698528,446443-446449,38593856-38593862,565653-565659,
-    //     824824821-824824827,2121212118-2121212124";
-    //     // act
-    //     let parsed_ranges = parse_id_ranges(id_ranges);
-    //     // assert
-    //     assert_eq!(
-    //         parsed_ranges,
-    //         vec![
-    //             (11, 22),
-    //             (95, 115),
-    //             (998, 1012),
-    //             (1188511880, 1188511890),
-    //             (222220, 222224),
-    //             (1698522, 1698528),
-    //             (446443, 446449),
-    //             (38593856, 38593862),
-    //             (565653, 565659),
-    //             (824824821, 824824827),
-    //             (2121212118, 2121212124)
-    //         ]
-    //     )
-    // }
-    // #[test]
-    // fn find_invalid_ids_in_range_10_10() {
-    //     // arrange
-    //     let range = (10, 10);
-    //     // act
-    //     let invalid_ids = find_invalid_ids(range);
-    //     // assert
-    //     assert_eq!(invalid_ids, [])
-    // }
-    // #[test]
-    // fn find_invalid_ids_in_range_111_111() {
-    //     // arrange
-    //     let range = (111, 111);
-    //     // act
-    //     let invalid_ids = find_invalid_ids(range);
-    //     // assert
-    //     assert_eq!(invalid_ids, [])
-    // }
-    // #[test]
-    // fn find_invalid_ids_in_range_11_11() {
-    //     // arrange
-    //     let range = (11, 11);
-    //     // act
-    //     let invalid_ids = find_invalid_ids(range);
-    //     // assert
-    //     assert_eq!(invalid_ids, [11])
-    // }
-    // #[test]
-    // fn find_invalid_ids_in_range_11_22() {
-    //     // arrange
-    //     let range = (11, 22);
-    //     // act
-    //     let invalid_ids = find_invalid_ids(range);
-    //     // assert
-    //     assert_eq!(invalid_ids, [11])
-    // }
-    // #[test]
-    // fn find_invalid_ids_in_range_1_2() {
-    //     // arrange
-    //     let range = (1, 2);
-    //     // act
-    //     let invalid_ids = find_invalid_ids(range);
-    //     // assert
-    //     assert_eq!(invalid_ids, [])
-    // }
 }
