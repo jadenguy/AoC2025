1100d5e4ed6160c2c0204fb459658c25c94dde06
Alfredo Inostroza <jadenguy@gmail.com>
2025-12-21 21:29:41 -0800

    fixed issue with repeat results where it could be 2,4,or 8 repeated digits

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,10 +1,38 @@
+use std::collections::HashSet;
+
 pub fn find_invalid_ids_lexicographically(range: &str, verbose: bool) -> Vec<u64> {
     let (first, last) = range.trim().split_once("-").unwrap();
-    let mut results: Vec<Vec<u64>> = vec![];
-    for divisor in 2..=last.len() {
-        results.push(get_repeats(range, verbose, first, last, divisor));
+    let mut results: HashSet<u64> = HashSet::new();
+    if verbose {
+        println!("checking {}; ", range)
     }
-    results.into_iter().flatten().collect()
+
+    for divisor in 2..=last.len() {
+        if verbose {
+            // print!("divisor {}; ", divisor)
+        }
+        let value = get_repeats(range, false, first, last, divisor);
+        if verbose {
+            // if value.len() == 0 {
+            //     print!(" no values")
+            // }
+            print!(
+                "{} ",
+                value
+                    .iter()
+                    .map(|y| y.to_string())
+                    .collect::<Vec<String>>()
+                    .join(", ")
+            );
+        }
+        for v in value {
+            results.insert(v);
+        }
+    }
+    if verbose {
+        println!("")
+    }
+    results.iter().map(|x| x.to_owned()).collect()
 }
 pub fn find_invalid_ids_lexicographically_by_two(range: &str, verbose: bool) -> Vec<u64> {
     let (first, last) = range.trim().split_once("-").unwrap();
@@ -21,13 +49,18 @@ pub fn find_invalid_ids_lexicographically_by_two(range: &str, verbose: bool) -> 
     get_repeats(range, verbose, first, last, 2)
 }
 fn get_repeats(range: &str, verbose: bool, first: &str, last: &str, divisor: usize) -> Vec<u64> {
-    let start_dup_range = lexicographical_lowest_bound(first, divisor, verbose);
-    let end_dup_range = lexicographical_upper_bound(last, divisor, verbose);
+    let start_dup_range = lexicographical_lowest_bound(first, divisor);
+    let end_dup_range = lexicographical_upper_bound(last, divisor);
     let end_digits = end_dup_range.parse::<u64>().unwrap();
     let start_digits = start_dup_range.parse::<u64>().unwrap();
     if end_digits < start_digits {
         if verbose {
-            println!("{} has no invalid ids in divisor {}", range, divisor)
+            println!(
+                "{} has no invalid ids",
+                range,
+                // start_digits.to_string().repeat(divisor),
+                // end_digits.to_string().repeat(divisor)
+            )
         }
         return vec![];
     }
@@ -36,7 +69,7 @@ fn get_repeats(range: &str, verbose: bool, first: &str, last: &str, divisor: usi
         .collect();
     if verbose {
         if !result.is_empty() {
-            print!("{} has invalid IDs in divisor {} ", range, divisor);
+            print!("{} has invalid IDs ", range);
 
             print!(
                 "{}",
@@ -51,32 +84,19 @@ fn get_repeats(range: &str, verbose: bool, first: &str, last: &str, divisor: usi
     };
     result
 }
-fn lexicographical_lowest_bound(num_str: &str, divisor: usize, verbose: bool) -> String {
+fn lexicographical_lowest_bound(num_str: &str, divisor: usize) -> String {
     let len = num_str.len();
     let chunk = len / divisor;
     if len < divisor {
         // if there aren't enough chunks, return a safe default
         //   of a single 1
-        // if verbose {
-        //     print!(
-        //         "smallest chunk possible for divisor {} is {}; ",
-        //         divisor,
-        //         1.to_string().repeat(divisor)
-        //     )
-        // }
+
         return 1.to_string();
     }
     if len % divisor != 0 {
         // if there are uneven chunks, return the lowest possible chunk as the first chunk
         let ret = String::from("1") + &"0".repeat(len - (chunk) - 1);
-        // if verbose {
-        //     print!(
-        //         "smallest chunk bigger than {} digits for divisor {} for this len is {}; ",
-        //         len,
-        //         divisor,
-        //         ret.to_string().repeat(divisor)
-        //     )
-        // }
+
         ret
     } else {
         let (highest_chunk, _min, max) = get_chunks(num_str, chunk);
@@ -84,23 +104,19 @@ fn lexicographical_lowest_bound(num_str: &str, divisor: usize, verbose: bool) ->
             false => (highest_chunk.parse::<u64>().unwrap_or_default() + 1).to_string(),
             true => highest_chunk.to_string(),
         };
-        // if verbose {
-        //     print!(
-        //         "smallest chunk bigger than {} digits for divisor {} for this len is {}; ",
-        //         len,
-        //         divisor,
-        //         ret.to_string().repeat(divisor)
-        //     )
-        // }
+
         ret
     }
 }
-fn lexicographical_upper_bound(num_str: &str, divisor: usize, verbose: bool) -> String {
+fn lexicographical_upper_bound(num_str: &str, divisor: usize) -> String {
     if num_str.len() < divisor {
         0.to_string()
     } else if num_str.len() % divisor == 0 {
         let (highest_chunk, min, _max) = get_chunks(num_str, num_str.len() / divisor);
-        get_lowest_upper_bound(divisor, verbose, highest_chunk, min)
+        match highest_chunk == min {
+            true => highest_chunk.to_string(),
+            false => (highest_chunk.parse::<u64>().unwrap_or_default() - 1).to_string(),
+        }
     } else {
         "9".repeat(num_str.len() / divisor)
     }
@@ -116,19 +132,4 @@ fn get_chunks(num_str: &str, chunk_size: usize) -> (String, String, String) {
     let min = digit_groups.iter().min().unwrap().to_string();
     let max = digit_groups.iter().max().unwrap().to_string();
     (highest_chunk, min, max)
-}
-fn get_lowest_upper_bound(
-    divisor: usize,
-    verbose: bool,
-    highest_chunk: String,
-    min: String,
-) -> String {
-    let result = match highest_chunk == min {
-        true => highest_chunk.to_string(),
-        false => (highest_chunk.parse::<u64>().unwrap_or_default() - 1).to_string(),
-    };
-    if verbose {
-        print!("upper range found as {}; ", result.repeat(divisor),);
-    }
-    result
 }
