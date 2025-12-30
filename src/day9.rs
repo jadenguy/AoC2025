20e4b3779aa0f13836cb86d3ca34d2678aa14c17
Alfredo Inostroza <jadenguy@gmail.com>
2025-12-30 12:34:37 -0800

    day9p1 test successfully

--- src/day9.rs
+++ src/day9.rs
@@ -1,1 +1,43 @@
-pub fn func() {}
+pub fn furthest_tiles(tiles: Vec<Coordinate>) -> Option<(Coordinate, Coordinate, i64)> {
+    let mut pairwise_areas: Vec<(Coordinate, Coordinate, i64)> = Vec::new();
+    for first in 0..tiles.len() {
+        let a = tiles[first];
+        for second in (first + 1)..tiles.len() {
+            let b = tiles[second];
+            let area_calc = (1 + (a.x - b.x).abs()) * (1 + (a.y - b.y).abs());
+            pairwise_areas.push((a, b, area_calc));
+        }
+    }
+    pairwise_areas.sort_by_key(|x| -x.2);
+
+    if pairwise_areas.len() > 0 {
+        let result = pairwise_areas.remove(0);
+        return Some(result);
+    }
+    None
+}
+pub fn parse_tiles(tile_strings: Vec<&str>) -> Vec<Coordinate> {
+    tile_strings
+        .iter()
+        .filter_map(|s| {
+            if let Some((a, b)) = s.split_once(",")
+                && let Ok(x) = a.parse::<i64>()
+                && let Ok(y) = b.parse::<i64>()
+            {
+                return Some(Coordinate { x: x, y: y });
+            }
+            None
+        })
+        .collect()
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, PartialOrd, Ord)]
+pub struct Coordinate {
+    pub x: i64,
+    pub y: i64,
+}
+impl PartialEq<(i64, i64)> for Coordinate {
+    fn eq(&self, other: &(i64, i64)) -> bool {
+        self.x == other.0 && self.y == other.1
+    }
+}
