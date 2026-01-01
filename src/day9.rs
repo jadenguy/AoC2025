8961962299f5f665a2f514adb2aad909084d840c
Alfredo Inostroza <jadenguy@gmail.com>
2026-01-01 15:26:40 -0800

    i'm going for a flood fill of tiles. that strategy should work.

--- src/day9.rs
+++ src/day9.rs
@@ -1,24 +1,38 @@
+use std::collections::HashSet;
+
+type LineSegment = (Coordinate, Coordinate);
 pub fn furthest_red_green_tiles(tiles: &Vec<Coordinate>) -> Option<(Coordinate, Coordinate, i64)> {
     let len = tiles.len();
     if len == 0 {
         return None;
     }
-
+    let green_tiles = get_green_tiles(tiles);
     let pairwise_areas = get_all_pairs_sorted_descending(tiles);
     // let largest = pairwise_areas[0];
     // find_segments_hitting_point(line_segments, largest);
     'bounding_box_search: for (a, b, dist) in pairwise_areas {
-        println!("Bounding Box {},{} {},{} Area {}", a.x, a.y, b.x, b.y, dist);
-        let (box_min_x, box_max_x) = if a.x < b.x { (a.x, b.x) } else { (b.x, a.x) };
-        let (box_min_y, box_max_y) = if a.y < b.y { (a.y, b.y) } else { (b.y, a.y) };
-        illustrate(tiles, box_min_x, box_max_x, box_min_y, box_max_y);
+        println!(
+            "Bounding Box {},{} {},{} Area {}",
+            a.row, a.col, b.row, b.col, dist
+        );
+        let (box_min_row, box_max_row) = if a.row < b.row {
+            (a.row, b.row)
+        } else {
+            (b.row, a.row)
+        };
+        let (box_min_col, box_max_col) = if a.col < b.col {
+            (a.col, b.col)
+        } else {
+            (b.col, a.col)
+        };
+        illustrate(tiles, box_min_row, box_max_row, box_min_col, box_max_col);
         let piercing_box_indexes: Vec<Coordinate> = tiles
             .iter()
             .filter_map(|p| {
                 // x inside box
-                let x_ok = box_min_x < p.x && p.x < box_max_x;
-                let y_ok = box_min_y < p.y && p.y < box_max_y;
-                if x_ok && y_ok {
+                let row_ok = box_min_row < p.row && p.row < box_max_row;
+                let col_ok = box_min_col < p.col && p.col < box_max_col;
+                if row_ok && col_ok {
                     let coordinate = p.to_owned();
                     return Some(coordinate);
                 }
@@ -29,7 +43,7 @@ pub fn furthest_red_green_tiles(tiles: &Vec<Coordinate>) -> Option<(Coordinate, 
             for p in piercing_box_indexes {
                 println!(
                     " {},{} to {},{} pierced by {},{}",
-                    a.x, a.y, b.x, b.y, p.x, p.y
+                    a.row, a.col, b.row, b.col, p.row, p.col
                 )
             }
             continue 'bounding_box_search;
@@ -39,21 +53,73 @@ pub fn furthest_red_green_tiles(tiles: &Vec<Coordinate>) -> Option<(Coordinate, 
     None
 }
 
+fn get_green_tiles(tiles: &[Coordinate]) -> Vec<Coordinate> {
+    let mut green: Vec<Coordinate> = Vec::new();
+    let mut lines: HashSet<LineSegment> = HashSet::new();
+
+    let (row_list, col_list): (Vec<i64>, Vec<i64>) = tiles.iter().map(|t| (t.row, t.col)).unzip();
+
+    let min_row = *row_list.iter().min().unwrap();
+    let min_col = *col_list.iter().min().unwrap();
+    let max_row = *row_list.iter().max().unwrap();
+    let max_col = *col_list.iter().max().unwrap();
+    let mut checked: HashSet<Coordinate> = HashSet::new();
+    let mut unchecked: HashSet<Coordinate> = HashSet::new();
+    for row in min_row..=max_row {
+        for col in min_col..=max_col {
+            unchecked.insert(Coordinate { row, col });
+        }
+    }
+    let mut left_most: LineSegment = (
+        Coordinate {
+            row: min_row,
+            col: max_col,
+        },
+        Coordinate {
+            row: max_row,
+            col: max_col,
+        },
+    );
+    for index in 0..tiles.len() {
+        let a = tiles[index];
+        let b = match index {
+            bi if bi + 1 == tiles.len() => tiles[0],
+            _ => tiles[index + 1],
+        };
+        lines.insert((a, b));
+        if
+            a.col <  left_most.0.col
+            && a.col == b.col
+            && a.row - a.col.abs() > 2
+        {
+            left_most = (a, b) match
+            {
+
+            };
+        }
+    }
+    green
+}
+
 fn illustrate(
     tiles: &[Coordinate],
-    box_min_x: i64,
-    box_max_x: i64,
-    box_min_y: i64,
-    box_max_y: i64,
+    box_min_row: i64,
+    box_max_row: i64,
+    box_min_col: i64,
+    box_max_col: i64,
 ) {
-    let (x_list, y_list): (Vec<i64>, Vec<i64>) = tiles.iter().map(|t| (t.x, t.y)).unzip();
-    let max_x = *x_list.iter().max().unwrap() + 1;
-    let max_y = *y_list.iter().max().unwrap() + 1;
-    for y in 0..=max_y {
-        for x in 0..=max_x {
-            if tiles.contains(&Coordinate { x: x, y: y }) {
+    let (rowl_list, col_list): (Vec<i64>, Vec<i64>) = tiles.iter().map(|t| (t.row, t.col)).unzip();
+    let max_row = *rowl_list.iter().max().unwrap() + 1;
+    let max_col = *col_list.iter().max().unwrap() + 1;
+    for row in 0..=max_row {
+        for col in 0..=max_col {
+            if tiles.contains(&Coordinate { row, col }) {
                 print!("#")
-            } else if box_min_x <= x && x <= box_max_x && box_min_y <= y && y <= box_max_y {
+            } else if box_min_row <= row
+                && row <= box_max_row
+                && box_min_col <= col
+                && col <= box_max_col
+            {
                 print!("O")
             } else {
                 print!(".")
@@ -77,7 +143,7 @@ fn get_all_pairs_sorted_descending(tiles: &Vec<Coordinate>) -> Vec<(Coordinate, 
         let a = tiles[first];
         for second in (first + 1)..tiles.len() {
             let b = tiles[second];
-            let area_calc = (1 + (a.x - b.x).abs()) * (1 + (a.y - b.y).abs());
+            let area_calc = (1 + (a.row - b.row).abs()) * (1 + (a.col - b.col).abs());
             pairwise_areas.push((a, b, area_calc));
         }
     }
@@ -89,10 +155,10 @@ pub fn parse_tiles(tile_strings: Vec<&str>) -> Vec<Coordinate> {
         .iter()
         .filter_map(|s| {
             if let Some((a, b)) = s.split_once(",")
-                && let Ok(x) = a.parse::<i64>()
-                && let Ok(y) = b.parse::<i64>()
+                && let Ok(col) = a.parse::<i64>()
+                && let Ok(row) = b.parse::<i64>()
             {
-                return Some(Coordinate { x: x, y: y });
+                return Some(Coordinate { row, col });
             }
             None
         })
@@ -100,20 +166,15 @@ pub fn parse_tiles(tile_strings: Vec<&str>) -> Vec<Coordinate> {
 }
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, PartialOrd, Ord)]
 pub struct Coordinate {
-    pub x: i64,
-    pub y: i64,
+    pub row: i64,
+    pub col: i64,
 }
 impl PartialEq<(i64, i64)> for Coordinate {
     fn eq(&self, other: &(i64, i64)) -> bool {
-        self.x == other.0 && self.y == other.1
+        self.row == other.0 && self.col == other.1
     }
 }
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, PartialOrd, Ord)]
-pub enum LineSegment {
-    Vertical { x: i64, y_bottom: i64, y_top: i64 },
-    Horizontal { x_left: i64, x_right: i64, y: i64 },
-    Error,
-}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -122,8 +183,10 @@ mod tests {
     fn test_furthest_tiles() {
         // arrange
         let tiles = parse_tiles(sample_data());
-        let expected: HashSet<Coordinate> =
-            HashSet::from_iter([Coordinate { x: 11, y: 1 }, Coordinate { x: 2, y: 5 }]);
+        let expected: HashSet<Coordinate> = HashSet::from_iter([
+            Coordinate { row: 11, col: 1 },
+            Coordinate { row: 2, col: 5 },
+        ]);
         // act
         let (a, b, area) =
             furthest_tiles(&tiles).expect("returned none but should have returned some");
@@ -136,7 +199,7 @@ mod tests {
         // arrange
         let tiles = parse_tiles(sample_data());
         let expected: HashSet<Coordinate> =
-            HashSet::from_iter([Coordinate { x: 9, y: 5 }, Coordinate { x: 2, y: 3 }]);
+            HashSet::from_iter([Coordinate { row: 9, col: 5 }, Coordinate { row: 2, col: 3 }]);
         // act
         let (a, b, area) =
             furthest_red_green_tiles(&tiles).expect("returned none but should have returned some");
