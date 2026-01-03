6fc2d6970960b34aa62982fe35d70176cdfc7053
Alfredo Inostroza <jadenguy@gmail.com>
2026-01-02 20:11:18 -0800

    clean up day10

--- src/day10.rs
+++ src/day10.rs
@@ -1,18 +1,5 @@
-// --- Day 10: Factory ---
+use std::iter;
 
-// Just across the hall, you find a large factory. Fortunately, the Elves here have plenty of time to decorate. Unfortunately, it's because the factory machines are all offline, and none of the Elves can figure out the initialization procedure.
-
-// The Elves do have the manual for the machines, but the section detailing the initialization procedure was eaten by a Shiba Inu. All that remains of the manual are some indicator light diagrams, button wiring schematics, and joltage requirements for each machine.
-
-// For example:
-
-// [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-// [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
-// [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
-
-use std::{fmt, iter};
-
-// The manual describes one machine per line. Each line contains a single indicator light diagram in [square brackets], one or more button wiring schematics in (parentheses), and joltage requirements in {curly braces}.
 type Indicator = u8;
 type ButtonDefinition = Vec<usize>;
 type Joltage = usize;
@@ -78,15 +65,6 @@ pub fn parse_machine_instructions(d: &str) -> MachineDefinition {
         joltage,
     }
 }
-// To start a machine, its indicator lights must match those shown in the diagram, where . means off and # means on. The machine has the number of indicator lights shown, but its indicator lights are all initially off.
-
-// So, an indicator light diagram like [.##.] means that the machine has four indicator lights which are initially off and that the goal is to simultaneously configure the first light to be off, the second light to be on, the third to be on, and the fourth to be off.
-
-// You can toggle the state of indicator lights by pushing any of the listed buttons. Each button lists which indicator lights it toggles, where 0 means the first light, 1 means the second light, and so on. When you push a button, each listed indicator light either turns on (if it was off) or turns off (if it was on). You have to push each button an integer number of times; there's no such thing as "0.5 presses" (nor can you push a button a negative number of times).
-
-// So, a button wiring schematic like (0,3,4) means that each time you push that button, the first, fourth, and fifth indicator lights would all toggle between on and off. If the indicator lights were [#.....], pushing the button would change them to be [...##.] instead.
-
-// Because none of the machines are running, the joltage requirements are irrelevant and can be safely ignored.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub struct MachineState {
     pub lit_indicators: Vec<Indicator>,
@@ -124,9 +102,6 @@ fn push_botton(new_indicators: &mut [u8], items: &[usize]) {
         new_indicators[*light_index] = (1 + new_indicators[*light_index]) % 2;
     }
 }
-
-// You can push each button as many times as you like. However, to save on time, you will need to determine the fewest total presses required to correctly configure all indicator lights for all machines in your list.
-
 pub fn find_min_presses(m: &MachineState) -> usize {
     let len = m.instructions.buttons.len();
     let max_button_state: usize = 1 << (len);
@@ -152,67 +127,6 @@ pub fn find_min_presses(m: &MachineState) -> usize {
     }
     min_presses
 }
-
-fn fun_name(
-    m: &MachineState,
-    button_press_state: usize,
-    buttons_pressed: &Vec<usize>,
-    x: &MachineState,
-) {
-    {
-        let button_press_state = button_press_state;
-        println!("{}", button_press_state);
-        let l = buttons_pressed
-            .iter()
-            .map(|n| n.to_string())
-            .collect::<Vec<_>>()
-            .join(", ");
-        println!("{}", l);
-        println!("");
-    };
-    println!(
-        "{} compared to {}",
-        x.lit_indicators
-            .iter()
-            .map(|n| n.to_string())
-            .collect::<Vec<_>>()
-            .join(", "),
-        m.instructions
-            .wanted_indicator_state
-            .iter()
-            .map(|n| n.to_string())
-            .collect::<Vec<_>>()
-            .join(", ")
-    );
-}
-
-// There are a few ways to correctly configure the first machine:
-
-// [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-// You could press the first three buttons once each, a total of 3 button presses.
-// You could press (1,3) once, (2,3) once, and (0,1) twice, a total of 4 button presses.
-// You could press all of the buttons except (1,3) once each, a total of 5 button presses.
-// However, the fewest button presses required is 2. One way to do this is by pressing the last two buttons ((0,2) and (0,1)) once each.
-
-// The second machine can be configured with as few as 3 button presses:
-
-// [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
-// One way to achieve this is by pressing the last three buttons ((0,4), (0,1,2), and (1,2,3,4)) once each.
-
-// The third machine has a total of six indicator lights that need to be configured correctly:
-
-// [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
-// The fewest presses required to correctly configure it is 2; one way to do this is by pressing buttons (0,3,4) and (0,1,2,4,5) once each.
-
-// So, the fewest button presses required to correctly configure the indicator lights on all of the machines is 2 + 3 + 2 = 7.
-
-// Analyze each machine's indicator light diagram and button wiring schematics. What is the fewest button presses required to correctly configure the indicator lights on all of the machines?
-
-// To begin, get your puzzle input.
-
-// Answer:
-
-// You can also [Share] this puzzle.
 pub fn x() {}
 #[cfg(test)]
 mod tests {
@@ -222,14 +136,11 @@ mod tests {
         // arrange
         let machines: Vec<MachineState> = sample_data()
             .iter()
-            // .skip(1)
             .map(|d| MachineState::from_instructions(&parse_machine_instructions(d)))
-            // .take(1)
             .collect();
         // act
         let actual: Vec<usize> = machines.iter().map(|m| find_min_presses(m)).collect();
         // assert
-        // assert_eq!(actual, [3])
         assert_eq!(actual, [2, 3, 2])
     }
     #[test]
